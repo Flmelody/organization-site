8f09a24ef6785b7c3df0fa3c982afa1b19a0f988
mysteriousman <justmysteriousman@163.com>
2023-07-19 20:19:34 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-fonts'
+  ],
+  css: [
+    '@/assets/css/main.css',
+  ],
 })
